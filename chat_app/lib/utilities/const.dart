2c74d11fd76c62543aa1c7162f8bfb06d040554e
Isagani Lapira <lapiraisagani@gmail.com>
2024-03-08 15:09:37 +0800

    Design welcome page with rive animation

---
+++ chat_app/lib/utilities/const.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const LinearGradient kLinearGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+    Color(0xFF04D2FF),
+    Color(0xFF55F2D6),
+  ],
+);
+
+const TextStyle kTitleStyle = TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 32.0,
+  color: Colors.white,
+);
