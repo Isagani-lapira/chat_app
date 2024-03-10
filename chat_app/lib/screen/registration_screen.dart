02c9c5ba14f7c3d0e8f9b77ce99fe50fa58022d1
Isagani Lapira <lapiraisagani@gmail.com>
2024-03-10 14:33:11 +0800

    Add: registration page and process

--- chat_app/lib/screen/registration_screen.dart
+++ chat_app/lib/screen/registration_screen.dart
@@ -1,4 +1,8 @@
+import 'package:chat_app/widgets/chatnow_button.dart';
 import 'package:flutter/material.dart';
+import '../utilities/const.dart';
+import '../widgets/textfield.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -9,8 +13,72 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  late String emailAddress;
+  late String password;
+  late FirebaseAuth _auth;
+
+  @override
+  void initState() {
+    super.initState();
+    _auth = FirebaseAuth.instance;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(gradient: kLinearGradient()),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign out',
+                style: kTitleStyle,
+                textAlign: TextAlign.center,
+              ),
+              ChatTextField(
+                label: 'Email address',
+                iconData: Icons.person,
+                onChanged: (value) {
+                  emailAddress = value;
+                },
+              ),
+              ChatTextField(
+                label: 'Password',
+                iconData: Icons.lock,
+                isObsured: true,
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: PrimaryButton(
+                  label: 'Register',
+                  pressed: () async {
+                    try {
+                      // create account
+                      UserCredential newUser =
+                          await _auth.createUserWithEmailAndPassword(
+                              email: emailAddress, password: password);
+
+                      if (newUser != null) {
+                        print('pumasok');
+                      } else {
+                        print('ayaw parin');
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
