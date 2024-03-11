f01f740b3a65b54fc2b7ede5d0d27e8c9ad9535f
Isagani Lapira <lapiraisagani@gmail.com>
2024-03-11 16:20:42 +0800

    Add text field and sign out of user in chat room

--- chat_app/lib/screen/chatroom.dart
+++ chat_app/lib/screen/chatroom.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/textfield.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatRoom extends StatefulWidget {
@@ -9,18 +11,56 @@ class ChatRoom extends StatefulWidget {
 }
 
 class _ChatRoomState extends State<ChatRoom> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Isagani',
+          'Room',
           textAlign: TextAlign.center,
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: const Color(0xFF04D2FF),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _auth.signOut();
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.close),
+          )
+        ],
       ),
-      body: Column(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            color: Colors.black,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                const Expanded(
+                  flex: 4,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter your message',
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color(0xFF04D2FF), width: 2.0),
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.send))
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
