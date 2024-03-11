dbca6b64bd2b1aabd61f79b56ac97abaeb4109b7
Isagani Lapira <lapiraisagani@gmail.com>
2024-03-11 16:21:17 +0800

    Remove: debug logo

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -19,6 +19,7 @@ class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => const WelcomeScreen(),
