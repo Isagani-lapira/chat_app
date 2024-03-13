c67a6a953fee5bb39fd0ea892003f79369be9365
Isagani Lapira <lapiraisagani@gmail.com>
2024-03-13 13:18:10 +0800

    Refractor: messagebubble to widget

--- chat_app/lib/screen/chatroom.dart
+++ chat_app/lib/screen/chatroom.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../widgets/widget_bubble.dart';
+
 final db = FirebaseFirestore.instance;
 
 class ChatRoom extends StatefulWidget {
@@ -132,68 +134,5 @@ class _ChatRoomState extends State<ChatRoom> {
         print(e);
       }
     }
-  }
-}
-
-class MessageBubble extends StatelessWidget {
-  const MessageBubble({
-    super.key,
-    required this.sender,
-    required this.message,
-    required this.isSender,
-  });
-
-  final String sender;
-  final String message;
-  final bool isSender;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 10.0,
-        horizontal: 25.0,
-      ),
-      child: Column(
-        crossAxisAlignment:
-            isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-        children: [
-          Text(
-            sender,
-            style: const TextStyle(
-              fontSize: 15.0,
-              color: Colors.black54,
-            ),
-          ),
-          const SizedBox(height: 5.0),
-          Material(
-            color: isSender
-                ? Colors.blue
-                : const Color.fromARGB(255, 202, 234, 235),
-            elevation: 5.0,
-            borderRadius: isSender
-                ? const BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
-                    bottomLeft: Radius.circular(30.0))
-                : const BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
-                    bottomRight: Radius.circular(30.0)),
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 25.0,
-              ),
-              child: Text(
-                message,
-                style: TextStyle(
-                  color: isSender ? Colors.white : Colors.black54,
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
   }
 }
