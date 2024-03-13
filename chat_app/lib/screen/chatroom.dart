6c7db4c7fc0e1995d7a04af3c593629088036f1c
Isagani Lapira <lapiraisagani@gmail.com>
2024-03-13 13:16:07 +0800

    Add: stream messaging retrieval

--- chat_app/lib/screen/chatroom.dart
+++ chat_app/lib/screen/chatroom.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+final db = FirebaseFirestore.instance;
 
 class ChatRoom extends StatefulWidget {
   static const String id = "chatroom.screen";
@@ -13,12 +16,10 @@ class ChatRoom extends StatefulWidget {
 class _ChatRoomState extends State<ChatRoom> {
   String message = '';
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final db = FirebaseFirestore.instance;
   late var currentUser;
-
+  final TextEditingController _controller = TextEditingController();
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     currentUser = _auth.currentUser;
   }
@@ -36,9 +37,8 @@ class _ChatRoomState extends State<ChatRoom> {
         actions: [
           IconButton(
             onPressed: () {
-              // _auth.signOut();
-              // Navigator.pop(context);
-              getMessages();
+              _auth.signOut();
+              Navigator.pop(context);
             },
             icon: const Icon(Icons.close),
           )
@@ -51,6 +51,39 @@ class _ChatRoomState extends State<ChatRoom> {
           Container(
             color: Colors.black,
           ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: StreamBuilder(
+                  stream: db
+                      .collection('messages')
+                      .orderBy('timestamp')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const CupertinoActivityIndicator();
+                    }
+
+                    final data = snapshot.data?.docs.reversed;
+                    List<Widget> messages = [];
+                    for (var message in data!) {
+                      String sender = message['sender'];
+                      String text = message['text'];
+
+                      messages.add(MessageBubble(
+                        sender: sender,
+                        message: text,
+                        isSender: currentUser.email == sender,
+                      ));
+                    }
+
+                    return ListView(
+                      reverse: true,
+                      children: messages,
+                    );
+                  }),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Row(
@@ -58,6 +91,7 @@ class _ChatRoomState extends State<ChatRoom> {
                 Expanded(
                   flex: 4,
                   child: TextField(
+                      controller: _controller,
                       decoration: const InputDecoration(
                         hintText: 'Enter your message',
                         focusedBorder: UnderlineInputBorder(
@@ -68,8 +102,14 @@ class _ChatRoomState extends State<ChatRoom> {
                       onChanged: (value) => {message = value}),
                 ),
                 IconButton(
-                    onPressed: () => sendMessage(message),
-                    icon: const Icon(Icons.send))
+                  onPressed: () {
+                    sendMessage(message);
+                    // reset textfield
+                    _controller.clear();
+                    message = '';
+                  },
+                  icon: const Icon(Icons.send),
+                )
               ],
             ),
           )
@@ -85,6 +125,7 @@ class _ChatRoomState extends State<ChatRoom> {
           {
             'sender': currentUser.email,
             'text': message,
+            'timestamp': DateTime.now(),
           },
         );
       } catch (e) {
@@ -92,12 +133,67 @@ class _ChatRoomState extends State<ChatRoom> {
       }
     }
   }
+}
 
-  void getMessages() async {
-    await for (var collection in db.collection('messages').snapshots()) {
-      for (var data in collection.docs) {
-        print(data.data());
-      }
-    }
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({
+    super.key,
+    required this.sender,
+    required this.message,
+    required this.isSender,
+  });
+
+  final String sender;
+  final String message;
+  final bool isSender;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 25.0,
+      ),
+      child: Column(
+        crossAxisAlignment:
+            isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            sender,
+            style: const TextStyle(
+              fontSize: 15.0,
+              color: Colors.black54,
+            ),
+          ),
+          const SizedBox(height: 5.0),
+          Material(
+            color: isSender
+                ? Colors.blue
+                : const Color.fromARGB(255, 202, 234, 235),
+            elevation: 5.0,
+            borderRadius: isSender
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0))
+                : const BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0)),
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: Text(
+                message,
+                style: TextStyle(
+                  color: isSender ? Colors.white : Colors.black54,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
